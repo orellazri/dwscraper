1c46258d394909782db2bd03ddda8f9369a4a1b1
Orel <ryiseld@gmail.com>
2023-02-25 10:03:14 +0200

    Tidy

--- src/main.rs
+++ src/main.rs
@@ -52,9 +52,4 @@ fn main() {
             }
         }
     }
-
-    // let document = fetch_document();
-    // let last_issue_number =
-    //     find_last_issue_number(&document).expect("failed to find last issue link");
-    // download_issue(last_issue_number).expect("failed to download issue");
 }
