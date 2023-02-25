b0dfa586ca4f7ee2e392a1672bff70b4c004d785
Orel <ryiseld@gmail.com>
2023-02-25 09:24:08 +0200

    Add clap and comand to download issue

---
+++ src/cli.rs
@@ -0,0 +1,12 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Subcommand)]
+pub enum Command {
+    Download { issue: i32 },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
+mod cli;
 mod document;
 mod issues;
 
+use clap::Parser;
+use cli::Command;
 use issues::download_issue;
 
-use crate::{document::fetch_document, issues::find_last_issue_number};
+use crate::{cli::Cli, document::fetch_document, issues::find_last_issue_number};
 
 fn main() {
-    let document = fetch_document();
-    let last_issue_number =
-        find_last_issue_number(&document).expect("failed to find last issue link");
-    download_issue(last_issue_number).expect("failed to download issue");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Download { issue } => {
+            download_issue(issue).expect("failed to download issue");
+        }
+    }
+
+    // let document = fetch_document();
+    // let last_issue_number =
+    //     find_last_issue_number(&document).expect("failed to find last issue link");
+    // download_issue(last_issue_number).expect("failed to download issue");
 }
